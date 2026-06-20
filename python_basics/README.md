08e888e05942a4ee2b9dbfd73fb3367eb718612a
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-06-20 13:25:00 +0530

    docs:added comment.

--- python_basics/README.md
+++ python_basics/README.md
@@ -1,5 +1,7 @@
 ## Funtion
 Function is a reusable block of code that can be used when it is called to perform a specific task.
 
-``` def greet()
-        print("hello")```
+``` python
+    def greet()
+        print("hello") 
+```
--- python_basics/basic.py
+++ python_basics/basic.py
@@ -1,3 +1,7 @@
 print("hello world")
-name = input("Enter your name ")
+
+# Ask user there name
+name = input("Enter your name ") # '=' here is assignment operator , it copies the return valur of right to left.
+# Greet user 
 print(f"hello,{name}")
+print("hello,",name)
