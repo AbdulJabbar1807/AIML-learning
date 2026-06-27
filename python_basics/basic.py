63328c729cb73d99668c4c732d7ae4510e0d614a
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-06-27 20:44:22 +0530

    feat:dictionaries in python.

--- python_basics/basic.py
+++ python_basics/basic.py
@@ -9,3 +9,21 @@ print("hello,",name)
 print("hello ", name, sep='???')
 print("hello",end='')
 print(name)
+
+
+# Dictionaries in python.
+students = [
+    {"name":"Abdul","branch":"CSE","year":"1st"},
+    {"name":"Mohammed","branch":"AIML","year":"2nd"},
+    {"name":"Ali","branch":"CY","year":"3rd"},
+]
+for student in students:
+    print(student["name"],student["branch"],student["year"])
+
+
+    
+
+
+
+    
+
