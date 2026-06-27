2702c22f5315d31530acc864fa000cebf743ebde
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-06-27 19:00:40 +0530

    feat:while loop in python.

--- loops/while_loop.py
+++ loops/while_loop.py
@@ -1,9 +1,24 @@
-i = int(input("What's i? "))
-while i > 0:
-    print("python")
-    i -= 1
+# # while-loop
+# i = int(input("What's i? "))
+# while i > 0:
+#     print("python")
+#     i -= 1
     
-i = int(input("What's i? "))
-while i < 5:
-    print("python")
-    i += 1
+# i = int(input("What's i? "))
+# while i < 5:
+#     print("python")
+#     i += 1
+    
+# Validating user input
+n = int(input("What's n? "))
+while True:
+    if n < 0:
+        n = int(input("What's n? "))
+        break
+    else:
+        break
+while n < 3:
+    print("*")
+    n += 1
+    
+    
