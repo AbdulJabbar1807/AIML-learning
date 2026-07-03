4cdafdd68a6e552ca79b97e1cb7bec621af6f2a8
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-07-03 22:59:42 +0530

    feat: loops and dicts.

---
+++ python_basics/loops+dicts.py
@@ -0,0 +1,13 @@
+marks = {
+    "Math": 89,
+    "Science": 92,
+    "English": 76
+}
+for subject,mark in marks.items():
+    print(f"{subject} -> {mark}")
+    
+for subject,mark in marks.items():
+    print(subject)
+    
+for subject,mark in marks.items():
+    print(mark)
