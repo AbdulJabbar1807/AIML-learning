a4ebd8519c4bdfbf5886ef8c346162444803d984
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-07-03 22:43:28 +0530

    feat:dictionaries in python.

---
+++ python_basics/dicts.py
@@ -0,0 +1,21 @@
+name = {"Abdul":"Jabbar"}
+print(name["Abdul"])
+
+info = {"name":"Abdul Jabbar",
+        "Age":21,
+        "City":"Ahmedabad",
+        "course":"Python"
+    }
+
+info["College"] = "BTU"
+
+print(info["name"],info["Age"])
+print(info["course"])
+print(info["College"])
+print(info)
+
+info["Age"] = 22
+print(info["Age"])
+
+del info["course"]
+print(info)
