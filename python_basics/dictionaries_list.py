0598e0879be949a98970d6182168ef02cb8b0d96
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-07-04 20:54:33 +0530

    feat:dictionaries in list.

---
+++ python_basics/dictionaries_list.py
@@ -0,0 +1,11 @@
+student_details = [
+    {"name":"Abdul","Age":20},
+    {"name":"Jabbar","Age":21},
+    {"name":"Khokhar","Age":22}
+]
+for st in student_details:
+    print(st["name"])
+    
+for age in student_details:
+    print(age["Age"])
+    
