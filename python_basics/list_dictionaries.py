dd9fe961c2ac04d29da6c43bea0f4f5d6d7516f6
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-07-06 09:50:35 +0530

    feat:iteration over lists in dictionaries.

--- python_basics/list_dictionaries.py
+++ python_basics/list_dictionaries.py
@@ -1,12 +1,24 @@
 # lists in dictionaries
-student = {
-    "name":"Abdul Jabbar",
-    "skills":["Python","C++","Java","SQL"]
-}
-print(student["skills"][3])
-student["skills"].append("Machine learning")
-for st in student["skills"]:
-    print(st)
+
+# student = {
+#     "name":"Abdul Jabbar",
+#     "skills":["Python","C++","Java","SQL"]
+# }
+# print(student["skills"][3])
+# student["skills"].append("Machine learning")
+# for st in student["skills"]:
+#     print(st)
     
-count = len(student["skills"])
-print(count)
+# count = len(student["skills"])
+# print(count)
+
+doctors = {
+    "year":2026,
+    "name1":["David","MBBS","MD","DM"],
+    "name2":["John","MBBS","MS"]
+}
+
+print(f"Year: {doctors['year']}")
+
+for detail in doctors["name1"]:
+    print(detail,end=" ")
