b6984b13d2b71d595d9c644c632f0a68c76ab4c5
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-07-12 15:40:58 +0530

    feat:greet.py

---
+++ libraries/greet.py
@@ -0,0 +1,12 @@
+def main():
+    hello("world")
+    goodbye("world")
+
+def hello(n):
+    print(f"hello,{n}")
+    
+def goodbye(n):
+    print(f"goodbye {n}")
+    
+if __name__ == "__main__":
+    main()
