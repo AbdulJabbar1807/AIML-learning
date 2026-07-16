3ea404633b6b500ba6feaea99a402301d39087ce
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-07-16 18:24:00 +0530

    feat:corner checks for add_students functino.

--- test_sms/test_add_student.py
+++ test_sms/test_add_student.py
@@ -1,6 +1,12 @@
 from Project_01.student_management_system import add_student
     
-def test_add_student():
+def test_add_student_empty():
     test_list = []
     add_student(test_list,"Abdul Jabbar")
     assert test_list == [{"id":1,"name":"Abdul Jabbar"}]
+    
+def test_add_student():
+    test_list = [{"id":1,"name":"Abdul"}]
+    add_student(test_list,"Abdul Jabbar")
+    assert test_list == [{"id":1,"name":"Abdul"},{"id":2,"name":"Abdul Jabbar"}]
+    
