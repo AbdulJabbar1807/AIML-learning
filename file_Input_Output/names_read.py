3bd1e0fab52550a6b1924fb2c0b1d957a185ed46
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-07-19 11:04:42 +0530

    feat:read file Input/Output program.

--- file_Input_Output/names_read.py
+++ file_Input_Output/names_read.py
@@ -1,10 +1,4 @@
-names = input("Enter your name: ")
-
-with open("names_read.txt","a") as file:
-    file.write(f"{names}\n")
-    
 with open("names_read.txt") as file:
-    lines = file.readlines()
+    for line in file:
+        print(f"hello,{line}".rstrip())
     
-for line in lines:
-    print(f"hello,{line}".rstrip())
