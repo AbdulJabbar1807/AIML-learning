4737476e08b785cded14a9a48ffa0aac22518a25
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-07-29 21:16:59 +0530

    Basic example of class and object in python.

---
+++ object-oriented_programming/class_objects_2.py
@@ -0,0 +1,14 @@
+class Book: # class
+    def __init__(self,id,title,author) -> None: # constructor
+        # self - its a built in variable in python,with it python automatically passes object as the first argument.
+        self.id = id    # -> atomic.id = id        #instance variable or Attribute
+        self.title = title      #instance variable or Attribute
+        self.author = author    # instance variable or Attribute
+        
+    def display(self):   # -> display(atomic)   # Method-functions it can perform.
+        print(f"Id: {self.id}") # -> atomic.id
+        print(f"Title: {self.title}") # -> atomic.title
+        print(f"Author: {self.author}") # -> atomic.author
+        
+atomic = Book(102,"Atomic Habits","James clear") # object or instance
+atomic.display()
