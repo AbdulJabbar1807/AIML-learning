0a46383b75232b7d6f21ee70a50f83322be8f492
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-08-16 17:31:37 +0530

    items class.

--- Project_02/items.py
+++ Project_02/items.py
@@ -1,17 +1,43 @@
-class Student:
-    def __init__(self,id,name,email):
-        self.id = id
+import csv
+
+class Item:
+    all = []
+    discount_rate = 0.5
+    def __init__(self,name: str,price: float,quantity: int=0):        
         self.name = name
-        self.email = email
+        assert price >=0, f"Price: {price} should be greater than zero."
+        self.price = price
+        assert price >=0, f"Quantity: {quantity} should be greater than zero."
+        self.quantity = quantity
         
-    def __str__(self) -> str:
-        return f"id {self.id},name {self.name},email {self.email}"    
+        Item.all.append(self)
         
-id = input("enter id: ")
-name = input("enter name: ")
-email = input("enter email address: ")
-
-student = Student(id,name,email)
-
-print(student.id)
-
+    def __str__(self):
+        return f"Item name: {self.name},Item price: {self.price},Item quantity: {self.quantity}"
+    
+    def __repr__(self):
+        return f"Item('{self.name}',{self.price},{self.quantity})"
+    
+    def calculate_price(self):
+        return self.price * self.quantity
+    
+    def discount_price(self):
+        self.price = self.price * self.discount_rate
+        return self.price
+    
+    @classmethod
+    def instantiate_csv(cls):
+        with open("items.csv",'r') as file:
+            reader = csv.DictReader(file)
+            items = list(reader)
+            
+        for item in items:
+            Item(
+                name = item.get('name'),
+                price = int(item.get('price')),
+                quantity = int(item.get('quantity'))
+            )
+        
+# item_1 = Item() # object with no instance attributes
+Item.instantiate_csv()
+print(Item.all)
